44a1ca616a3d7c2585d8652976558f829e4ab6c2
ANHTT <anh_tt@septeni-technology.jp>
2017-12-15 10:38:59 +0700

    temp

--- de/test/test.py
+++ de/test/test.py
@@ -14,20 +14,24 @@ sys.path.insert(0, '/home/sepml/workspace/neural_artistic_style/dataGenerator/SR
 
 from de.model.SRCNN import SRCNN
 import tensorflow as tf
+import de.data.image as dimage
+import de.util.IO as io
 
 #flags = tf.app.flags
 #flags.DEFINE_integer("epoch", 10, "Number of epoch [10]")
 #flags.DEFINE_integer("batch_size", 128, "The size of batch images [128]")
 #flags.DEFINE_integer("image_size", 3840, "The size of image to train [3840]")
 
-with tf.Session() as sess:
-    srcnn = SRCNN(sess,
-                  image_size = 3840,
-                  label_size = 3840,
-                  image_channel = 3,
-                  batch_size = 1,
-                  num_epoch = 10000,
-                  checkpoint_dir = '/checkpoint',
-                  sample_dir = '../../../Train')
-    
-    srcnn.train()
+io.read_dir("../../../Train", ['jpg','jpeg','bmp','png','JPEG'])
+
+#with tf.Session() as sess:
+#    srcnn = SRCNN(sess,
+#                  image_size = 3840,
+#                  label_size = 3840,
+#                  image_channel = 3,
+#                  batch_size = 1,
+#                  num_epoch = 10000,
+#                  checkpoint_dir = '/checkpoint',
+#                  sample_dir = '../../../Train')
+#    
+#    srcnn.train()
