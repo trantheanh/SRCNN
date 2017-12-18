9b2bee8cda3bba0e346eda150b0a92134797d622
ANHTT <anh_tt@septeni-technology.jp>
2017-12-18 11:18:18 +0700

    temp

--- de/model/SRCNN.py
+++ de/model/SRCNN.py
@@ -120,7 +120,7 @@ class SRCNN(object):
         
         batch_input = scipy.ndimage.interpolation.zoom(batch, (1.,1./(scale), 1./(scale), 1), prefilter = False)
         batch_input = scipy.ndimage.interpolation.zoom(batch_input, (1., scale/1., scale/1., 1), prefilter = False)
-        batch_label = scipy.ndimage.interpolation.zoom(batch, (1., 1., 1., 1), prefilter = False)
+        batch_label = batch
         
         return batch_input, batch_label
     
